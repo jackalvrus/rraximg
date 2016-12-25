cd48ea4b7a7d47b0d8580b49c29ad21e7ef61daa
Tom Hoppe <trhoppe@gmail.com>
2016-12-24 21:58:13 -0700

    Adding styled image page

--- image.php
+++ image.php
@@ -6,40 +6,38 @@
 <body>
     <?php include 'common/header.php'; ?>
     <div class="container">
-        <div class="image row">
+        <div class="image">
 
             <?php
             $target_file = htmlspecialchars($_GET['i']);
 
             if (!file_exists($target_file)) {
-                echo '<p>Yore file is not here. <a href="/img/">See all your images</p>';
+                echo '<div class="imageError"><p><strong>WAT DID YOU DO??? Your file is not here!</strong></p><p><a href="/img/">See all your images</p></div>';
             } else {
                 $directory = pathinfo($target_file, PATHINFO_DIRNAME);
                 $filename =  pathinfo($target_file, PATHINFO_FILENAME);
                 $extension =  pathinfo($target_file, PATHINFO_EXTENSION);
 
-                echo '<div style="clear:both; margin-bottom: 20px;"></div>';
-
-                echo '<div style="float: left; width: 60%; padding-right: 2%; max-width: 640px;">';
-                    echo '<img style="width: 100%; height: auto; max-width: 640px;" src="' . $directory . '/' . $filename . '-m.' . $extension . '">';
+                echo '<div class="imageSingle">';
+                echo '<img src="' . $directory . '/' . $filename . '-m.' . $extension . '">';
                 echo '</div>';
 
-                echo '<div style="float: left; width: 38%;">';
-                    echo '<div style="font-weight: bold;">Small (~200px)</div>';
-                    echo '<div>-Direct Image: <a href="http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-s.' . $extension . '">http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-s.' . $extension . '</a></div>';
-                    echo '<div>-Forum Embed: [img]http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-s.' . $extension . '[/img]</div>';
+                echo '<div class="imageLinks">';
+                    echo '<div class="imageType"><strong>Small (~200px)</strong></div>';
+                    echo '<div class="imageDirectLink"><strong>Direct Image:</strong> <span><a href="http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-s.' . $extension . '">http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-s.' . $extension . '</a></span></div>';
+                    echo '<div class="imageEmbed"><strong>Forum Embed:</strong> <span>[img]http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-s.' . $extension . '[/img]</span></div>';
 
-                    echo '<p style="font-weight: bold;">Medium (~600px)</p>';
-                    echo '<div>-Direct Image: <a href="http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-m.' . $extension . '">http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-m.' . $extension . '</a></div>';
-                    echo '<div>-Forum Embed: [img]http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-m.' . $extension . '[/img]</div>';
+                    echo '<div class="imageType"><strong>Medium (~600px)</strong></div>';
+                    echo '<div class="imageDirectLink"><strong>Direct Image:</strong> <span><a href="http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-m.' . $extension . '">http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-m.' . $extension . '</a></span></div>';
+                    echo '<div class="imageEmbed"><strong>Forum Embed:</strong> <span>[img]http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-m.' . $extension . '[/img]</span></div>';
 
-                    echo '<p style="font-weight: bold;">Large (~1200px)</p>';
-                    echo '<div>-Direct Image: <a href="http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-l.' . $extension . '">http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-l.' . $extension . '</a></div>';
-                    echo '<div>-Forum Embed: [img]http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-l.' . $extension . '[/img]</div>';
+                    echo '<div class="imageType"><strong>Large (~1200px)</strong></div>';
+                    echo '<div class="imageDirectLink"><strong>Direct Image:</strong> <span><a href="http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-l.' . $extension . '">http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-l.' . $extension . '</a></span></div>';
+                    echo '<div class="imageEmbed"><strong>Forum Embed:</strong> <span>[img]http://www.roadraceautox.com/img/' . $directory . '/' . $filename . '-l.' . $extension . '[/img]</span></div>';
 
-                    echo '<p style="font-weight: bold;">Original (~2000px)</p>';
-                    echo '<div>-Direct Image: <a href="http://www.roadraceautox.com/img/' . $target_file . '">http://www.roadraceautox.com/img/' . $target_file . '</a></div>';
-                    echo '<div>-Forum Embed: [img]http://www.roadraceautox.com/img/' . $target_file . '[/img]</div>';
+                    echo '<div class="imageType"><strong>Original (~2000px)</strong></div>';
+                    echo '<div class="imageDirectLink"><strong>Direct Image:</strong> <span><a href="http://www.roadraceautox.com/img/' . $target_file . '">http://www.roadraceautox.com/img/' . $target_file . '</a></span></div>';
+                    echo '<div class="imageEmbed"><strong>Forum Embed:</strong> <span>[img]http://www.roadraceautox.com/img/' . $target_file . '[/img]</span></div>';
                 echo '</div>';
             }
             ?>
